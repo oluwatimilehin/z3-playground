017559877a0f276f79a578778b0636981fa37422
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-02-15 21:20:36 +0000

    Precondition synthesis first pass

--- precondition_synthesis.py
+++ precondition_synthesis.py
@@ -93,30 +93,35 @@ def precondition_synthesis(
     positive_example: Dict[BitVecRef, BitVecVal],
     negative_examples: List[Dict[BitVecRef, BitVecVal]],
     bitwidth: int,
+    max_conjuctions: int = 2,
 ):
     sym_vars = [
         BitVec(f"symb_{i}", bitwidth) for i in range(1, len(positive_example) + 1)
     ]
     expression_sketches = generate_expressions(sym_vars, bitwidth, 0)
 
-    values = list(positive_example.keys()) + [BitVecVal(0, 8), BitVecVal(1, 8)]
+    values = list(positive_example.keys()) + [
+        BitVecVal(0, bitwidth),
+        BitVecVal(1, bitwidth),
+    ]
     combinations = [
         combo
         for combo in list(itertools.product(values, repeat=len(positive_example)))
-        if len(set(combo)) > 1 and set(combo) != {BitVecVal(0, 8), BitVecVal(1, 8)}
+        if len(set(combo)) > 1
+        and set(combo) != {BitVecVal(0, bitwidth), BitVecVal(1, bitwidth)}
     ]
 
-    print(f"expression sketches: {expression_sketches}")
-    print(f"combinations: {combinations}")
+    # print(f"expression sketches: {expression_sketches}")
+    # print(f"combinations: {combinations}")
 
     precondition_candidates = []
     # Is power of 2:
-    for subst_sketch, val in positive_example.items():
+    for const, val in positive_example.items():
         power_of_2_solver = Solver()
         power_of_2_solver.add(val & (val - 1) == 0)
 
         if power_of_2_solver.check().r == Z3_L_TRUE:
-            precondition_candidates.append(subst_sketch & (subst_sketch - 1) == 0)
+            precondition_candidates.append(const & (const - 1) == 0)
 
     # TODO: If we have no valid candidates with a single expression, consider joining candidates up to a limit
     for sketch in expression_sketches:
@@ -152,17 +157,11 @@ def precondition_synthesis(
                 lt_solver.add(negative_expr >= 0)
                 lte_solver.add(negative_expr > 0)
 
-            is_lte = lte_solver.check().r == Z3_L_TRUE
-            is_gte = gte_solver.check().r == Z3_L_TRUE
+            if lte_solver.check().r == Z3_L_TRUE:
+                precondition_candidates.append(subst_sketch <= 0)
 
-            if is_lte or is_gte:
-                if is_lte:
-                    precondition_candidates.append(subst_sketch <= 0)
-
-                if is_gte:
-                    precondition_candidates.append(subst_sketch >= 0)
-
-                continue
+            if gte_solver.check().r == Z3_L_TRUE:
+                precondition_candidates.append(subst_sketch >= 0)
 
             if eq_solver.check().r == Z3_L_TRUE:
                 precondition_candidates.append(subst_sketch == 0)
@@ -174,33 +173,86 @@ def precondition_synthesis(
                 precondition_candidates.append(subst_sketch < 0)
 
     valid_candidates = []
-    for candidate in precondition_candidates:
-        verifier = Solver()
+    for i in range(1, max_conjuctions + 1):
+        candidates = precondition_candidates
+        # print(f"Attempt {i} at finding candidates")
+        if i > 1:
+            conjuctions = [
+                combo
+                for combo in list(itertools.product(candidates, repeat=i))
+                if len(set(combo)) > 1
+            ]
+            candidates = [And(*combo) for combo in conjuctions]
 
-        verifier.add(And(candidate, Not(expr)))
+        for candidate in candidates:
+            verifier = Solver()
 
-        print(candidate)
-        if verifier.check().r == Z3_L_FALSE:
-            valid_candidates.append(candidate)
+            verifier.add(And(candidate, Not(expr)))
 
-        print(verifier.model())
+            print(candidate)
+            if verifier.check().r == Z3_L_FALSE:
+                valid_candidates.append(candidate)
+                continue
 
-    # TODO:
-    # - i also need to come up with negative examples
-    # now verify the candidates properly; i.e. does the expression always hold true for all values of a and b
+            # print(verifier.model())
+        if valid_candidates:
+            break
 
     return valid_candidates
 
 
+def get_negative_examples(expr: BitVecRef, consts: List[BitVecRef], num: int = 3):
+    s = Solver()
+    s.add(Not(expr))
+
+    results: List[Dict[BitVecRef, BitVecVal]] = []
+    for _ in range(num):
+        if s.check().r == Z3_L_FALSE:
+            break  # Means that there are no constants for which the expression is not satisfied
+
+        model = s.model()
+        negative_example = {c: model[c] for c in consts}
+        results.append(negative_example)
+
+        for c, val in negative_example.items():
+            s.add(c != val)
+
+    return results
+
+
 if __name__ == "__main__":
     bitwidth = 8
-    x, c1, c2 = BitVecs("x c1 c2", bitwidth)
+    x, y, c1, c2, c3 = BitVecs("x y c1 c2 c3", bitwidth)
 
+    expr = LShR(x << c1, c2) << c3 == x & LShR(-1 << c1, c2) << c3
+    negative_examples = get_negative_examples(expr=expr, consts=[c1, c2, c3], num=3)
+    print(f"negative examples: {negative_examples}")
     print(
         precondition_synthesis(
-            expr=(x >> c1) * c2 == x * (c2 >> c1),
-            positive_example={c1: BitVecVal(1, bitwidth), c2: BitVecVal(8, bitwidth)},
-            negative_examples=[],
+            expr,
+            positive_example={
+                c1: BitVecVal(8, bitwidth),
+                c2: BitVecVal(16, bitwidth),
+                c3: BitVecVal(8, bitwidth),
+            },
+            negative_examples=negative_examples,
             bitwidth=bitwidth,
         )
     )
+
+    # This fails to generate: IsPowerOf2(𝐶1 ) ∧ (𝐶1 − 𝐶2 ) = 1;
+    expr = (c2 != (c2 & x)) & ULT(x, c1) == ULT(x, c2)
+    negative_examples = get_negative_examples(expr=expr, consts=[c1, c2], num=3)
+    print(f"negative examples: {negative_examples}")
+    print(
+        precondition_synthesis(
+            expr,
+            positive_example={c1: BitVecVal(16, bitwidth), c2: BitVecVal(15, bitwidth)},
+            negative_examples=negative_examples,
+            bitwidth=bitwidth,
+        )
+    )
+
+    # This should have no negative examples, meaning it doesn't require preconditions
+    expr = (x + c1) - (y + c2) == x - y + (c1 - c2)
+    assert get_negative_examples(expr=expr, consts=[c1, c2], num=3) == []
